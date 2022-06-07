bac0f9bfbf30e8930170bd18b71cd2157797a0c2
Francois Blackburn <fblackburn@wazo.community>
2022-06-07 08:12:13 -0400

    verify.sh: avoid to silently fail if-clause
    why: when empty string, test command can raise this kind of error
    ```
    [: !=: unary operator expected
    ```
    but will not return error code and will not stop script

--- verify.sh
+++ verify.sh
@@ -22,7 +22,7 @@ wait_for_bootstrap_complete
 
 echo -n 'Validating bootstrap exit status... '
 BOOTSTRAP_EXIT_CODE=$(docker-compose ps --status exited --format json bootstrap | jq --raw-output .[0].ExitCode)
-if [ $BOOTSTRAP_EXIT_CODE -ne 0 ]; then
+if [ "$BOOTSTRAP_EXIT_CODE" -ne 0 ]; then
     echo 'FAILED'
     exit 1
 fi
@@ -53,7 +53,7 @@ AUTH_CODE=$(curl \
   --write-out "%{http_code}" \
   'https://localhost:8443/api/auth/0.1/status')
 
-if [ $AUTH_CODE -ne 200 ]; then
+if [ "$AUTH_CODE" -ne 200 ]; then
   echo 'FAILED'
   exit 1
 fi
@@ -69,7 +69,7 @@ PROVD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/provd/0.2/status')
 
-if [ $(echo $PROVD_STATUS | jq --raw-output .rest_api) != 'ok' ]; then
+if [ "$(echo $PROVD_STATUS | jq --raw-output .rest_api)" != 'ok' ]; then
   echo 'FAILED'
   exit 1
 fi
@@ -85,11 +85,11 @@ WEBHOOKD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/webhookd/1.0/status')
 
-if [ $(echo $WEBHOOKD_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $WEBHOOKD_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consume)'
   exit 1
 fi
-if [ $(echo $WEBHOOKD_STATUS | jq --raw-output .master_tenant.status) != 'ok' ]; then
+if [ "$(echo $WEBHOOKD_STATUS | jq --raw-output .master_tenant.status)" != 'ok' ]; then
   echo 'FAILED (master_tenant)'
   exit 1
 fi
@@ -105,11 +105,11 @@ DIRD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/dird/0.1/status')
 
-if [ $(echo $DIRD_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $DIRD_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consume)'
   exit 1
 fi
-if [ $(echo $DIRD_STATUS | jq --raw-output .rest_api.status) != 'ok' ]; then
+if [ "$(echo $DIRD_STATUS | jq --raw-output .rest_api.status)" != 'ok' ]; then
   echo 'FAILED (rest_api)'
   exit 1
 fi
@@ -126,7 +126,7 @@ AMID_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/amid/1.0/action/ping')
 
-if [ $(echo $AMID_STATUS | jq --raw-output .[].Response) != 'Success' ]; then
+if [ "$(echo $AMID_STATUS | jq --raw-output .[].Response)" != 'Success' ]; then
   echo 'FAILED (ping action)'
   exit 1
 fi
@@ -142,15 +142,15 @@ CALL_LOGD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/call-logd/1.0/status')
 
-if [ $(echo $CALL_LOGD_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $CALL_LOGD_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consume)'
   exit 1
 fi
-if [ $(echo $CALL_LOGD_STATUS | jq --raw-output .task_queue.status) != 'ok' ]; then
+if [ "$(echo $CALL_LOGD_STATUS | jq --raw-output .task_queue.status)" != 'ok' ]; then
   echo 'FAILED (task_queue)'
   exit 1
 fi
-if [ $(echo $CALL_LOGD_STATUS | jq --raw-output .service_token.status) != 'ok' ]; then
+if [ "$(echo $CALL_LOGD_STATUS | jq --raw-output .service_token.status)" != 'ok' ]; then
   echo 'FAILED (service_token)'
   exit 1
 fi
@@ -181,19 +181,19 @@ CHATD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/chatd/1.0/status')
 
-if [ $(echo $CHATD_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $CHATD_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consume)'
   exit 1
 fi
-if [ $(echo $CHATD_STATUS | jq --raw-output .rest_api.status) != 'ok' ]; then
+if [ "$(echo $CHATD_STATUS | jq --raw-output .rest_api.status)" != 'ok' ]; then
   echo 'FAILED (rest_api)'
   exit 1
 fi
-if [ $(echo $CHATD_STATUS | jq --raw-output .presence_initialization.status) != 'ok' ]; then
+if [ "$(echo $CHATD_STATUS | jq --raw-output .presence_initialization.status)" != 'ok' ]; then
   echo 'FAILED (presence_initialization)'
   exit 1
 fi
-if [ $(echo $CHATD_STATUS | jq --raw-output .master_tenant.status) != 'ok' ]; then
+if [ "$(echo $CHATD_STATUS | jq --raw-output .master_tenant.status)" != 'ok' ]; then
   echo 'FAILED (master_tenant)'
   exit 1
 fi
@@ -209,11 +209,11 @@ PHONED_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/phoned/0.1/status')
 
-if [ $(echo $PHONED_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $PHONED_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consume)'
   exit 1
 fi
-if [ $(echo $PHONED_STATUS | jq --raw-output .service_token.status) != 'ok' ]; then
+if [ "$(echo $PHONED_STATUS | jq --raw-output .service_token.status)" != 'ok' ]; then
   echo 'FAILED (service_token)'
   exit 1
 fi
@@ -236,7 +236,7 @@ WEBSOCKETD_CODE=$(curl \
   "https://localhost:8443/api/websocketd/?token=$TOKEN&version=2")
 set -e
 
-if [ $WEBSOCKETD_CODE -ne 101 ]; then
+if [ "$WEBSOCKETD_CODE" -ne 101 ]; then
   echo 'FAILED'
   exit 1
 fi
@@ -252,15 +252,15 @@ CALLD_STATUS=$(curl \
   --header "X-Auth-Token: $TOKEN" \
   'https://localhost:8443/api/calld/1.0/status')
 
-if [ $(echo $CALLD_STATUS | jq --raw-output .ari.status) != 'ok' ]; then
+if [ "$(echo $CALLD_STATUS | jq --raw-output .ari.status)" != 'ok' ]; then
   echo 'FAILED (ari)'
   exit 1
 fi
-if [ $(echo $CALLD_STATUS | jq --raw-output .bus_consumer.status) != 'ok' ]; then
+if [ "$(echo $CALLD_STATUS | jq --raw-output .bus_consumer.status)" != 'ok' ]; then
   echo 'FAILED (bus_consumer)'
   exit 1
 fi
-if [ $(echo $CALLD_STATUS | jq --raw-output .service_token.status) != 'ok' ]; then
+if [ "$(echo $CALLD_STATUS | jq --raw-output .service_token.status)" != 'ok' ]; then
   echo 'FAILED (service_token)'
   exit 1
 fi
@@ -277,7 +277,7 @@ AGENTD_CODE=$(curl \
   --write-out "%{http_code}" \
   'https://localhost:8443/api/agentd/1.0/agents')
 
-if [ $AGENTD_CODE -ne 200 ]; then
+if [ "$AGENTD_CODE" -ne 200 ]; then
   echo 'FAILED'
   exit 1
 fi
